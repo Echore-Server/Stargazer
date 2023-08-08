8eb155439de34fc09934beb5ca95702adfc5898a
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-08-08 20:48:53 +0900

    `ModifierApplier`がフィルターと追加モディファイヤのサポート

--- src/ConsecutiveModifierApplier.php
+++ src/ConsecutiveModifierApplier.php
@@ -4,10 +4,20 @@ namespace Echore\Stargazer;
 
 class ConsecutiveModifierApplier extends ModifierApplier {
 
-	public function apply(float|int $value, array $modifiers, ?int &$totalAbsolute = null, bool $absolute = true): float {
+	public function apply(float|int $value, array $modifiers, array $applyModifiers = [], ?int &$totalAbsolute = null, bool $absolute = true): float {
 		$finalAbsolute = 0;
 		foreach ($modifiers as $modifier) {
-			$value *= $modifier->multiplier;
+			if (!$modifier->testFilter($value)) {
+				continue;
+			}
+
+			$diff = $value * ($modifier->multiplier - 1.0);
+
+			foreach ($applyModifiers as $applyModifier) {
+				$diff = $this->apply($diff, [$applyModifier], absolute: $absolute);
+			}
+
+			$value += $diff;
 			$finalAbsolute += $modifier->absolute;
 		}
 
--- src/ModifierApplier.php
+++ src/ModifierApplier.php
@@ -7,10 +7,11 @@ abstract class ModifierApplier {
 	/**
 	 * @param int|float $value
 	 * @param Modifier[] $modifiers
+	 * @param Modifier[] $applyModifiers
 	 * @param int|null $totalAbsolute
 	 * @param bool $absolute
 	 * @return float
 	 */
-	abstract public function apply(int|float $value, array $modifiers, ?int &$totalAbsolute = null, bool $absolute = true): float;
+	abstract public function apply(int|float $value, array $modifiers, array $applyModifiers = [], ?int &$totalAbsolute = null, bool $absolute = true): float;
 
 }
