69c21b43671a54259713d9667c1905ba8c23f823
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-11-21 15:51:04 +0900

    `Stargazer`側でダメージの処理を行わないように

--- src/Main.php
+++ src/Main.php
@@ -9,9 +9,6 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 
 	protected function onEnable(): void {
-		$eventListener = new EventListener();
-
-		$this->getServer()->getPluginManager()->registerEvents($eventListener, $this);
 	}
 
 }
